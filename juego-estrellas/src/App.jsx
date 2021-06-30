1a57d714990ba48a0deca0acb7b2f9f1a24e7fc1
mauricio403 <mauriciomatango_@hotmail.com>
2021-06-30 13:40:47 -0500

    Squenme de Latam

---
+++ juego-estrellas/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../src/components/app.css'
+
+
+
+export const App = () => {
+  return (
+    <div>
+      <h1>Saquenme de Latinoamerica!</h1>
+    </div>
+  )
+}
